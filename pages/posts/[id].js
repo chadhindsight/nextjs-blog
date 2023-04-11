d35b5badabb769f74290f0088fbb15185345bf92
chad hinds <chadrickj8@gmail.com>
2023-04-11 13:51:56 -0400

    Add head

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,5 @@
 import Layout from '../../components/layout';
-
+import Head from 'next/head';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
@@ -22,11 +22,16 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
         <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
             {postData.title}
             <br />
             {postData.id}
             <br />
             {postData.date}
+            <br />
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     );
 }
