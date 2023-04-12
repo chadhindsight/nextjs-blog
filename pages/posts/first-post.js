a1a8d1f81925524f6c06c38b80148078d532adf7
chad hinds <chadrickj8@gmail.com>
2023-04-12 10:47:56 -0400

    lower case l

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/layout';
-
+// boom
 export default function FirstPost() {
     return (
         <Layout>
